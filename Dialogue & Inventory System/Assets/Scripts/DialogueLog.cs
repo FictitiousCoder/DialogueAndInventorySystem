1b9b90c1b58c3094ba7e48755b609011a64ec6da
FictitiousCoder <jonny_b@hotmail.no>
2016-08-24 16:50:35 +0200

    Re-grouping Scripts

---
+++ Dialogue & Inventory System/Assets/Scripts/DialogueLog.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogueLog : MonoBehaviour {
+
+    public Text logText;
+    public GameObject logBox;
+    public KeyCode toggleDialogueLog;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(toggleDialogueLog))
+        {
+            logBox.SetActive(!logBox.active);
+        }
+    }
+
+    // Adds text to the dialogue log and increments size
+    public void LogText(string text, string speaker)
+    {
+        if (speaker.Length > 0)
+        {
+            logText.text += ("<b>" + speaker + "</b>: ");
+        }
+        logText.text += (text + "\n\n");
+        RectTransform log = logText.gameObject.GetComponent<RectTransform>();
+        log.sizeDelta = new Vector2(log.sizeDelta.x, log.sizeDelta.y + 50);
+    }
+}
---
+++ Dialogue & Inventory System/Assets/Scripts/SitDown.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SitDown : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject sittingPlayer;
+
+    private bool inRange = false;
+    private bool sitting = false;
+
+
+    void Update()
+    {
+        if (inRange == true && sitting == false && Input.GetKeyDown(KeyCode.R))
+        {
+            Sit();  
+        }
+        else if (sitting == true && Input.GetKeyDown(KeyCode.R))
+        {
+            StandUp();
+        }
+    }
+
+    void Sit()
+    {
+        player.SetActive(false);
+        sittingPlayer.SetActive(true);
+        sitting = true;
+    }
+
+    void StandUp()
+    {
+        sittingPlayer.SetActive(false);
+        player.SetActive(true);
+        sitting = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inRange = false;
+        }
+    }
+}
